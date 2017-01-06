7d6f13b479fa1c2e485622432eb9767ff57a09f5
DESKTOP-ELIGO1N\Richi <richinel_jarbandhan@hotmail.com>
2017-01-07 00:33:41 +0100

    First commit of Casa Vastgoed =)

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Casa Vastgoed</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="assets/css/kube.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/font-awesome.css">
+
+	<script src="assets/js/jquery-3.1.1.min.js"></script>
+
+</head>
+<body>
+
+	<div class="nav">
+		<div class="nav_info">
+			<div class="wrapper">
+				<div class="phone">
+					<i class="fa fa-phone" aria-hidden="true"></i>
+					Bel direct:  06 - 87 65 72 46
+				</div>
+			</div>
+		</div>
+
+		<div class="wrapper">
+			<img src="assets/images/casavastgoed.png">
+		</div>
+	</div>
