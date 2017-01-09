281814f1f72b75c811730f58c30aa70321bb45c4
Richinel Jarbandhan <richinel_jarbandhan@hotmail.com>
2017-01-09 14:24:30 +0100

    Fixed the information section.

--- index.php
+++ index.php
@@ -9,6 +9,8 @@
 
 	<div class="main_info">
 		<div class="wrapper">
+			<h5>Our solution is your solution</h5>
+
 			<div class="row">
 				<div class="col">
 					Casa-vastgoed is een dynamisch bedrijf met veel specialistische kennis in huis op het gebied van makelaardij in onroerend goed. Wij werken intensief samen met onafhankelijke professionals die hun sporen hebben verdiend in het vakgebied van de financiële dienstverlening.
@@ -18,12 +20,59 @@
 				<div class="col margin_col">
 				 	Wilt u een woning aankopen en wenst u professionele begeleiding, dan kunt u ons inschakelen als aankopend makelaar. Indien u ons inschakelt kunt u ten alle tijde rekenen op persoonlijke en professionele begeleiding.
 					Overweegt u uw woning te verkopen of te verhuren dan kunt u ons bellen voor een gratis waardebepaling. Een eerste afspraak is altijd kosteloos en geheel vrijblijvend.<br>
-					<a href="#">Lees meer...</a>
 				</div>
 			</div>
 
 			<div class="btn">
-				Bekijk aanbod
+				Lees meer
+			</div>
+		</div>
+	</div>
+
+	<div class="information_blocks">
+		<div class="wrapper">
+			<div class="row between">
+				<div class="col">
+					<i class="fa fa-home" aria-hidden="true"></i>
+
+					<h4>Woning huren?</h4>
+						<hr>
+					<p>
+						Bent u geïnteresseerd in een woning op onze website? Wilt u meer informatie of een afspraak maken om de woning te bezichtigen? Neem dan contact met ons op per telefoon.
+					</p>
+
+					<div class="btn-sm">
+						Meer info
+					</div>
+				</div>
+
+				<div class="col">
+					<i class="fa fa-credit-card-alt" aria-hidden="true"></i>
+
+					<h4>Woning kopen?</h4>
+						<hr>
+					<p>
+						Het begint altijd met een enthousiast, goed en vrijblijvend kennismakingsgesprek en een gratis waarde bepaling. In een persoonlijk gesprek stellen wij ons graag aan u voor, luisteren naar uw wensen en beantwoorden we al uw vragen.
+					</p>
+
+					<div class="btn-sm">
+						Meer info
+					</div>
+				</div>
+
+				<div class="col">
+					<i class="fa fa-wrench" aria-hidden="true"></i>
+
+					<h4>Renovatie</h4>
+						<hr>
+					<p>
+						Ook renoveren wij woningen die wij vervolgens beschikbaar stellen om te huren of te kopen. Neem voor meer informatie contact met ons op.
+					</p>
+
+					<div class="btn-sm">
+						Meer info
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
